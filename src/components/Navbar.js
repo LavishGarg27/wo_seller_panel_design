2616e8d41aa0c3653ed34bbebba9713064b4a44c
Lavish Garg <lavishgarg2711@gmail.com>
2022-05-26 18:29:28 +0530

    components added and clean folders

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <>
+        <div className="navbar">
+            <h3>Logo</h3>
+            <div className="nav">
+                <div className="search">
+                    <div className="product">
+                        <p>Product Name</p> 
+                        <i class="fa fa-angle-down" aria-hidden="true"></i>
+                    </div>
+                    <input type="text" placeholder="Search..."/>
+                    <i class="fa fa-search" aria-hidden="true"></i>
+                </div>
+
+                <div className="menu">
+                <h1 className="feedback">
+                    Feedback
+                </h1>
+                <h1 className="language">
+                    English
+                </h1>
+                <i class="fa fa-bell-o" aria-hidden="true"></i>
+                <i class="fa fa-user-circle" aria-hidden="true"></i>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
